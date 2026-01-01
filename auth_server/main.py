3d652673b16b0ac366730f7c04f3c1ce0ae4a74f
Adi <adi@adiMac>
2026-01-01 20:45:35 +0200

    added rate_limit feature

--- auth_server/main.py
+++ auth_server/main.py
@@ -2,17 +2,19 @@ import time
 import sqlite3
 import uvicorn
 from typing import Optional
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, Request
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
 from pydantic import BaseModel
 
-# Import our new modules
 import config
 import security
 import database
 import loaders
 
 app = FastAPI(title="Auth Server")
-
+app.state.limiter = security.limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 class TOTPLoginRequest(BaseModel):
     username: str
@@ -31,21 +33,22 @@ class LoginRequest(BaseModel):
 
 
 @app.post("/register")
-def register(request: RegisterRequest, conn: sqlite3.Connection = Depends(database.get_db_conn)):
+@security.limiter.limit(security.get_limit_value)
+def register(req: RegisterRequest, request: Request, conn: sqlite3.Connection = Depends(database.get_db_conn)):
     cursor = conn.cursor()
 
-    if database.user_exists(request.username, cursor):
+    if database.user_exists(req.username, cursor):
         raise HTTPException(status_code=400, detail="Username already exists")
 
-    hash_data = security.get_password_hash(request.password, config.HASH_ALGO)
+    hash_data = security.get_password_hash(req.password, config.HASH_ALGO)
 
     try:
         database.insert_user_to_db(
-            username=request.username,
+            username=req.username,
             algo=config.HASH_ALGO,
             salt=hash_data["salt"],
             hash_val=hash_data["hash"],
-            totp_secret=request.totp_secret,
+            totp_secret=req.totp_secret,
             cursor=cursor,
             conn=conn
         )
@@ -56,13 +59,14 @@ def register(request: RegisterRequest, conn: sqlite3.Connection = Depends(databa
 
     return {
         "message": "User created successfully",
-        "username": request.username,
-        "totp_secret": request.totp_secret
+        "username": req.username,
+        "totp_secret": req.totp_secret
     }
 
 
 @app.post("/login")
-def login(request: LoginRequest, conn: sqlite3.Connection = Depends(database.get_db_conn)):
+@security.limiter.limit(security.get_limit_value)
+def login(req: LoginRequest, request: Request, conn: sqlite3.Connection = Depends(database.get_db_conn)):
     # For logs
     start_time = time.time()
 
@@ -71,12 +75,12 @@ def login(request: LoginRequest, conn: sqlite3.Connection = Depends(database.get
     login_success = False
     cursor = conn.cursor()
     # Gets user data
-    user = database.get_user(request.username, cursor)
+    user = database.get_user(req.username, cursor)
     hash_mode = user["algo_type"] if user else "Unknown"
     totp_secret = user["totp_secret"] if user else None
     totp_required = totp_enabled and totp_secret
 
-    if user and security.verify_password(request.password, user):
+    if user and security.verify_password(req.password, user):
         login_success = True
         result_str = "Password login | Login Successful"
         if totp_required:
@@ -86,7 +90,7 @@ def login(request: LoginRequest, conn: sqlite3.Connection = Depends(database.get
     latency_ms = (end_time - start_time) * 1000
 
     database.log_attempt(
-        username=request.username,
+        username=req.username,
         hash_mode=hash_mode,
         result=result_str,
         latency_ms=latency_ms,
@@ -104,7 +108,8 @@ def login(request: LoginRequest, conn: sqlite3.Connection = Depends(database.get
 
 
 @app.post("/login_totp")
-def login_totp(request: TOTPLoginRequest, conn: sqlite3.Connection = Depends(database.get_db_conn)):
+@security.limiter.limit(security.get_limit_value)
+def login_totp(req: TOTPLoginRequest, request: Request, conn: sqlite3.Connection = Depends(database.get_db_conn)):
     # For logs
     start_time = time.time()
 
@@ -112,12 +117,12 @@ def login_totp(request: TOTPLoginRequest, conn: sqlite3.Connection = Depends(dat
     login_success = False
 
     cursor = conn.cursor()
-    user = database.get_user(request.username, cursor)
+    user = database.get_user(req.username, cursor)
     hash_mode = user["algo_type"] if user else "Unknown"
     secret = user["totp_secret"] if user else None
 
     if user and secret:
-        if security.verify_totp(secret, request.totp_code):
+        if security.verify_totp(secret, req.totp_code):
             login_success = True
             result_str = "TOTP login | TOTP valid, login completed"
     else:
@@ -127,7 +132,7 @@ def login_totp(request: TOTPLoginRequest, conn: sqlite3.Connection = Depends(dat
     latency_ms = (end_time - start_time) * 1000
 
     database.log_attempt(
-        username=request.username,
+        username=req.username,
         hash_mode=hash_mode,
         result=result_str,
         latency_ms=latency_ms,
@@ -140,7 +145,7 @@ def login_totp(request: TOTPLoginRequest, conn: sqlite3.Connection = Depends(dat
     return {
         "message": "Login successful",
         "user_id": user["id"],
-        "username": request.username,
+        "username": req.username,
         "valid": True
     }
 
